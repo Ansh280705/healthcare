58fb6f293fe48e583a46e6656012459f40a300db
abhirajjaiswal010 <abhirajjaiswal500@gmail.com>
2025-12-31 00:46:32 +0530

    final changes

--- actions/onboarding.js
+++ actions/onboarding.js
@@ -202,4 +202,20 @@ export async function setUserRole(formData) {
     console.error("❌ Onboarding Error:", error);
     throw new Error("Failed to complete onboarding");
   }
+}export async function getCurrentUser() {
+  const { userId } = auth(); // NOT await
+
+  if (!userId) return null;
+
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        clerkUserId: userId,
+      },
+    });
+    return user;
+  } catch (error) {
+    console.error("Failed to get user information:", error);
+    return null;
+  }
 }
