7a2567902f5fe5d678eb7c1af4fd456a7096725c
abhirajjaiswal010 <abhirajjaiswal500@gmail.com>
2026-01-12 01:56:53 +0530

    mannual2.o

---
+++ app/robots.ts
@@ -0,0 +1,39 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: [
+          "/",
+          "/blog",
+          "/labs",
+          "/pricing",
+          "/about",
+          "/ContactUs",
+          "/help-center",
+          "/privacy-policy",
+          "/terms-and-conditions",
+          "/refund-policy",
+          "/cancellation-policy",
+          "/shipping-and-delivery",
+          "/legal-information",
+          "/team-devs",
+        ],
+        disallow: [
+          "/admin",
+          "/admin/",
+          "/doctor",
+          "/doctor/",
+          "/onboarding",
+          "/appointments",
+          "/prescriptions",
+          "/video-call",
+          "/doctors/",
+        ],
+      },
+    ],
+    sitemap: "https://www.doctordesk.co.in/sitemap.xml",
+  };
+}
